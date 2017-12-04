4ed9ecc1e9e2ae5a1707ceff54591373ab275b16
Shyam Kannan <csystic@gmail.com>
2017-12-04 13:54:20 +0530

    First commit

---
+++ r1/246313/submission/files/05_portofolio/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(),r,_(s,t,u,v,w,_(x,y,z,A),B,null,C,v,D,v,E,F,G,null,H,I,J,K,L,M,N,I),O,_(P,_()),Q,_(),R,_(S,[])),T,_(),U,_());}; 
+var b="url",c="05_portofolio.html",d="generationDate",e=new Date(1511868738187.65),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="b79bc3fe76874f1f986d97e47d0c4250",m="type",n="Axure:Page",o="name",p="05 Portofolio",q="notes",r="style",s="baseStyle",t="627587b6038d43cca051c114ac41ad32",u="pageAlignment",v="near",w="fill",x="fillType",y="solid",z="color",A=0xFFFFFFFF,B="image",C="imageHorizontalAlignment",D="imageVerticalAlignment",E="imageRepeat",F="auto",G="favicon",H="sketchFactor",I="0",J="colorStyle",K="appliedColor",L="fontName",M="Applied Font",N="borderWidth",O="adaptiveStyles",P="a",Q="interactionMap",R="diagram",S="objects",T="masters",U="objectPaths";
+return _creator();
+})());
